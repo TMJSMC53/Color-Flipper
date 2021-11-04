7b02fb03cb612a4697a2dbe52565e5863c5929f2
Triola Michelle Jackson <triolamichellejackson@Triolas-MacBook-Pro-2.local>
2021-11-04 15:06:50 -0600

    added the css styling and javascript to their respective files

---
+++ notes.md
@@ -0,0 +1,15 @@
+1. One button in the center of the page that says change color.
+2. Above it will say "background-color: color" when you click the button, a new color will cover the page.
+3. And the name of the color will go where the word color is.
+4. Use arrays, Math.floor, Math.random. Iterate through an array to select a random color to appear on the screen when you click the button.
+
+- create an array with various colors
+
+COOL FEATURES:
+
+1. Add a multicolor border around the click me button AND the color flipper title
+2. Add a transition to the button
+
+BONUS:
+
+1. Make 4 div boxes inside the project which all change seperate colors.
---
+++ src/script.js
@@ -0,0 +1,50 @@
+const colors = [
+  "#fec5bb",
+  "#fcd5ce",
+  "#fae1dd",
+  "#f8edeb",
+  "#e8e8e4",
+  "#d8e2dc",
+  "#ece4db",
+  "#ffe5d9",
+  "#ffd7ba",
+  "#fec89a",
+  "#ffadad",
+  "#ffd6a5",
+  "#fdffb6",
+  "#caffbf",
+  "#9bf6ff",
+  "#a0c4ff",
+  "#bdb2ff",
+  "#ffc6ff",
+  "#fffffc",
+  "#005f73",
+  "#0a9396",
+  "#94d2bd",
+  "#e9d8a6",
+  "#ee9b00",
+  "#ca6702",
+  "#bb3e03",
+  "#ae2012",
+  "#9b2226",
+  "#f6bd60",
+  "#f7ede2",
+  "#f5cac3",
+  "#84a59d",
+  "#f28482",
+];
+
+const colorDisplay = document.querySelector("#color-display");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+  const randomNum = generatedRandNumber();
+
+  document.body.style.backgroundColor = colors[randomNum];
+
+  colorDisplay.textContent = colors[randomNum];
+});
+
+const generatedRandNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+};
